fcb107d08750c0929b0b1ec6398daceda6e09bd4
Anna Perevedentseva <anna.pereved@gmail.com>
2025-07-11 23:06:02 +0200

    feat: add basic layout for error button component

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import './App.css';
 import { Component } from 'react';
 import Search from './components/Search/Search';
+import ErrorButton from './components/ErrorButton/ErrorButton';
 
 class App extends Component {
   render() {
     return (
       <>
         <Search />
+        <ErrorButton />
       </>
     );
   }
---
+++ src/components/ErrorButton/ErrorButton.tsx
@@ -0,0 +1,9 @@
+import { Component } from 'react';
+
+class ErrorButton extends Component {
+  render() {
+    return <button>Error</button>;
+  }
+}
+
+export default ErrorButton;
