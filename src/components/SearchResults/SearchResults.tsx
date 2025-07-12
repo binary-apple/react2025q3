742c93d484999b9ef48a7aa8803c86e763382d00
Anna Perevedentseva <anna.pereved@gmail.com>
2025-07-12 23:07:00 +0200

    feat: add fetching data from api

--- src/components/SearchResults/SearchResults.tsx
+++ src/components/SearchResults/SearchResults.tsx
@@ -1,6 +1,40 @@
 import { Component } from 'react';
 
-class SearchResults extends Component {
+type SearchResultsState = {
+  isLoading: boolean;
+  isError: boolean;
+  searchResults: unknown[];
+};
+
+class SearchResults extends Component<object, SearchResultsState> {
+  constructor(props: object) {
+    super(props);
+    this.state = {
+      isLoading: false,
+      isError: false,
+      searchResults: [],
+    };
+  }
+  async componentDidMount(): Promise<void> {
+    this.setState({ isLoading: true });
+    const response = await fetch(
+      'https://potterapi-fedeperin.vercel.app/en/characters',
+      {
+        method: 'GET',
+        headers: {
+          'content-type': 'application/json',
+        },
+      }
+    );
+    const results = await response.json();
+
+    if (response.ok) {
+      this.setState({ searchResults: results, isLoading: false });
+    } else {
+      this.setState({ isLoading: false, isError: true });
+    }
+  }
+
   render() {
     return <div>Search results</div>;
   }
