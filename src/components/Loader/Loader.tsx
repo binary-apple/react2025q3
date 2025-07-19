4b9c1e00b9d79384f8f0e2aaae67383ce925c4ef
Anna Perevedentseva <anna.pereved@gmail.com>
2025-07-19 17:54:09 +0200

    test: render loading state correctly

--- src/components/Loader/Loader.tsx
+++ src/components/Loader/Loader.tsx
@@ -3,7 +3,7 @@ import classes from './Loader.module.css';
 
 class Loader extends Component {
   render() {
-    return <div className={classes.loader}></div>;
+    return <div className={classes.loader} data-testid="loader"></div>;
   }
 }
 
---
+++ src/components/SearchResults/SearchResults.test.tsx
@@ -0,0 +1,36 @@
+import '@testing-library/jest-dom/vitest';
+import { render, screen } from '@testing-library/react';
+import { describe, expect, test } from 'vitest';
+import SearchResults from './SearchResults';
+
+describe('Search results: Loader ', () => {
+  const mockSearchResultsProps = {
+    isLoading: true,
+    isError: false,
+    searchResults: [],
+  };
+
+  test('does not render if isLoading === false', () => {
+    mockSearchResultsProps.isLoading = false;
+    render(
+      <SearchResults
+        isLoading={mockSearchResultsProps.isLoading}
+        isError={mockSearchResultsProps.isError}
+        searchResults={mockSearchResultsProps.searchResults}
+      />
+    );
+    expect(screen.queryByTestId('loader')).not.toBeInTheDocument();
+  });
+
+  test('renders if isLoading === true', () => {
+    mockSearchResultsProps.isLoading = true;
+    render(
+      <SearchResults
+        isLoading={mockSearchResultsProps.isLoading}
+        isError={mockSearchResultsProps.isError}
+        searchResults={mockSearchResultsProps.searchResults}
+      />
+    );
+    expect(screen.queryByTestId('loader')).toBeInTheDocument();
+  });
+});
