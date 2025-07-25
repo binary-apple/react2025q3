2363ab0ca37dbd3b552ebf2c705cf17b68e79ea6
Anna Perevedentseva <anna.pereved@gmail.com>
2025-07-25 11:18:32 +0200

    feat: add react router and skeletons for pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,19 @@
 import './App.css';
+import { BrowserRouter, Route, Routes } from 'react-router';
 import ErrorBoundary from './components/ErrorBoundary/ErrorBoundary';
 import MainPage from './pages/main/MainPage';
 
 function App() {
   return (
-    <ErrorBoundary>
-      <MainPage />
-    </ErrorBoundary>
+    <BrowserRouter>
+      <ErrorBoundary>
+        <Routes>
+          <Route path="/" element={<MainPage />} />
+          <Route path="/about" element={<div>About</div>} />
+          <Route path="*" element={<div>Not found</div>} />
+        </Routes>
+      </ErrorBoundary>
+    </BrowserRouter>
   );
 }
 
