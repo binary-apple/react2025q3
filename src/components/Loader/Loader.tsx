93820c286d35316837d4357819ab886c6adc7f92
Anna Perevedentseva <anna.pereved@gmail.com>
2025-07-25 10:37:34 +0200

    refactor: convert loader to functioal component

--- src/components/Loader/Loader.tsx
+++ src/components/Loader/Loader.tsx
@@ -1,10 +1,7 @@
-import { Component } from 'react';
 import classes from './Loader.module.css';
 
-class Loader extends Component {
-  render() {
-    return <div className={classes.loader} data-testid="loader"></div>;
-  }
+function Loader() {
+  return <div className={classes.loader} data-testid="loader"></div>;
 }
 
 export default Loader;
