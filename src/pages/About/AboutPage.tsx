c974727e3a7c6c6e928063e04e4c2f7739bad5d1
Anna Perevedentseva <anna.pereved@gmail.com>
2025-08-03 00:54:59 +0200

    refactor: style about page with tailwind classes

--- src/pages/About/AboutPage.tsx
+++ src/pages/About/AboutPage.tsx
@@ -1,34 +1,29 @@
-import classes from './AboutPage.module.css';
-
 function AboutPage() {
   return (
-    <div className={classes.wrapper}>
+    <div className="flex flex-col gap-5 max-w-[320px] text-lg">
       <div>
         Hi, my name is Anna and I am currently learning magic of React in
         RSSchool (Hogwarts in programming world). <br />
         Take a look into my code spellbook:{' '}
-        <a href="https://github.com/binary-apple" target="blank">
+        <a
+          href="https://github.com/binary-apple"
+          target="blank"
+          className="underline"
+        >
           github
         </a>
         .
       </div>
-      <div className={classes['school-info']}>
-        <div>
-          If you also want to become a frontend-wizard, don&apos;t wait for an
-          owl, just visit the{' '}
-          <a href="https://rs.school/courses/reactjs" target="blank">
-            website
-          </a>
-        </div>
-        <div>
-          <a href="https://rs.school/courses/reactjs" target="blank">
-            <img
-              className={classes.logo}
-              src="https://rs.school/_next/static/media/rss-logo.c19ce1b4.svg"
-              alt="Course logo"
-            ></img>
-          </a>
-        </div>
+      <div>
+        If you also want to become a frontend-wizard, don&apos;t wait for an
+        owl, just visit the{' '}
+        <a
+          href="https://rs.school/courses/reactjs"
+          target="blank"
+          className="underline"
+        >
+          website
+        </a>
       </div>
     </div>
   );
