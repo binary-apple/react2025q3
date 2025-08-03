2ad8314b09b9dc39c8153b83f7cdb05beab00415
Anna Perevedentseva <anna.pereved@gmail.com>
2025-08-03 13:00:12 +0200

    feat: add checkbox for dark theme

--- src/layouts/Layout.tsx
+++ src/layouts/Layout.tsx
@@ -5,32 +5,37 @@ function Layout() {
   return (
     <div className="flex flex-col gap-2.5 flex-grow items-center">
       <header className="flex flex-row justify-between w-[320px] py-2.5">
-        <NavLink
-          to="/"
-          className={({ isActive }) =>
-            isActive ? 'cursor-default text-primary font-bold' : ''
-          }
-          onClick={(e) => {
-            if (window.location.pathname === '/') {
-              e.preventDefault();
+        <nav className="flex gap-5">
+          <NavLink
+            to="/"
+            className={({ isActive }) =>
+              isActive ? 'cursor-default text-primary font-bold' : ''
             }
-          }}
-        >
-          Main
-        </NavLink>
-        <NavLink
-          to="/about"
-          className={({ isActive }) =>
-            isActive ? 'cursor-default text-primary font-bold' : ''
-          }
-          onClick={(e) => {
-            if (window.location.pathname === '/about') {
-              e.preventDefault();
+            onClick={(e) => {
+              if (window.location.pathname === '/') {
+                e.preventDefault();
+              }
+            }}
+          >
+            Main
+          </NavLink>
+          <NavLink
+            to="/about"
+            className={({ isActive }) =>
+              isActive ? 'cursor-default text-primary font-bold' : ''
             }
-          }}
-        >
-          About
-        </NavLink>
+            onClick={(e) => {
+              if (window.location.pathname === '/about') {
+                e.preventDefault();
+              }
+            }}
+          >
+            About
+          </NavLink>
+        </nav>
+        <label>
+          <input type="checkbox"></input> Dark mode
+        </label>
       </header>
       <main className="flex gap-2.5 flex-grow items-center">
         <Outlet />
