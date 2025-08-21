fa0f328c513db838d800e1610a802819dfab82a5
Anna Perevedentseva <anna.pereved@gmail.com>
2025-08-22 00:35:14 +0200

    tests: add app test

---
+++ src/App.test.tsx
@@ -0,0 +1,10 @@
+import { render, screen } from '@testing-library/react';
+import { describe, test, expect } from 'vitest';
+import App from './App';
+
+describe('App', () => {
+  test('should render "hello forms"', () => {
+    render(<App />);
+    expect(screen.getByText(/hello forms!/i)).toBeInTheDocument();
+  });
+});
