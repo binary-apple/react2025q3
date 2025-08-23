657c11c29664affc46e6d426a1a23638b611f259
Anna Perevedentseva <anna.pereved@gmail.com>
2025-08-24 00:59:25 +0200

    feat: add custom component and layout to forms for gender picker

--- src/components/forms/uncontrolled-form.tsx
+++ src/components/forms/uncontrolled-form.tsx
@@ -1,4 +1,4 @@
-import { Input, Checkbox } from '@/components/inputs';
+import { Input, Checkbox, GenderPicker } from '@/components/inputs';
 import Button from '@/components/button';
 
 function UncontrolledForm() {
@@ -9,6 +9,7 @@ function UncontrolledForm() {
         <Input placeholder="Name" label="Name" />
         <Input placeholder="Age" label="Age" type="number" />
         <Input placeholder="Email" label="Email" type="email" />
+        <GenderPicker />
         <Checkbox label="Accept T&C" />
         <div className="flex gap-2 justify-evenly">
           <Button type="submit">Submit</Button>
--- src/components/inputs/index.ts
+++ src/components/inputs/index.ts
@@ -1,3 +1,5 @@
 export { default as Input } from './input';
 
 export { default as Checkbox } from './checkbox';
+
+export { default as GenderPicker } from './gender-radio';
