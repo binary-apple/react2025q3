9fff11879ffc8336add7d4a88a64b693c9872a15
Anna Perevedentseva <anna.pereved@gmail.com>
2025-08-24 14:49:21 +0200

    feat: save input changes in controlled form

--- src/components/forms/controlled-form.tsx
+++ src/components/forms/controlled-form.tsx
@@ -33,11 +33,43 @@ function ControlledForm() {
     <div>
       <h2 className="font-bold m-2 text-xl">Controlled form</h2>
       <form className="flex flex-col gap-2">
-        <Input placeholder="Name" label="Name" />
-        <Input placeholder="Age" label="Age" type="number" />
-        <Input placeholder="Email" label="Email" type="email" />
+        <Input
+          placeholder="Name"
+          label="Name"
+          value={formState.name}
+          onChange={(e) =>
+            setFormState((prev) => ({ ...prev, name: e.target.value }))
+          }
+        />
+        <Input
+          placeholder="Age"
+          label="Age"
+          type="number"
+          value={formState.age}
+          onChange={(e) =>
+            setFormState((prev) => {
+              const value = e.target.value;
+              return { ...prev, age: value === '' ? '' : +value };
+            })
+          }
+        />
+        <Input
+          placeholder="Email"
+          label="Email"
+          type="email"
+          value={formState.email}
+          onChange={(e) =>
+            setFormState((prev) => ({ ...prev, email: e.target.value }))
+          }
+        />
         <GenderPicker />
-        <Checkbox label="Accept T&C" />
+        <Checkbox
+          label="Accept T&C"
+          checked={formState.terms}
+          onChange={(e) =>
+            setFormState((prev) => ({ ...prev, terms: e.target.checked }))
+          }
+        />
         <div className="flex gap-2 justify-evenly">
           <Button type="submit" onClick={onSubmit}>
             Submit
