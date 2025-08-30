ea67fc5423665c82f8059711a8dd26f34734abb0
Anna Perevedentseva <anna.pereved@gmail.com>
2025-08-30 23:29:01 +0200

    feat: add header with subtitle

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,10 @@ import MainPage from '@components/main-page';
 function App() {
   return (
     <>
-      <h1 className="text-5xl font-bold py-2">React performance</h1>
+      <header className="flex justify-between max-w-5xl mx-auto">
+        <h1 className="text-5xl font-bold py-2">React performance</h1>
+        <h2 className="text-5xl font-bold py-2">CO2 Emissions</h2>
+      </header>
       <MainPage />
     </>
   );
