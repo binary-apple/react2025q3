dcb00868f57b75b209260e072063928714feec4f
Anna Perevedentseva <anna.pereved@gmail.com>
2025-08-31 11:23:46 +0200

    feat: add styles to header

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import MainPage from '@components/main-page';
 function App() {
   return (
     <>
-      <header className="flex justify-between max-w-5xl mx-auto">
+      <header className="flex justify-between max-w-5xl mx-auto my-3">
         <h1 className="text-5xl font-bold py-2">React performance</h1>
         <h2 className="text-5xl font-bold py-2">CO2 Emissions</h2>
       </header>
