a081b7fc22d07bf21b2f9d25ca5b7b5cc474ce2c
Anna Perevedentseva <anna.pereved@gmail.com>
2025-09-01 01:08:15 +0200

    refactor: remove comments

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,6 @@
 import { createRoot } from 'react-dom/client';
 import '@/index.css';
 import App from '@/App';
-// import { StrictMode } from 'react';
 
 const root = document.getElementById('root');
 
@@ -9,8 +8,4 @@ if (!root) {
   throw new Error('#root element not found');
 }
 
-createRoot(root).render(
-  // <StrictMode>
-  <App />
-  // </StrictMode>
-);
+createRoot(root).render(<App />);
